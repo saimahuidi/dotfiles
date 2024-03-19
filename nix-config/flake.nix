91388b36a477101a6598f6ea8f42216a6b08ff6c
geoffrey <geoffrey2gong@gmail.com>
2024-03-20 01:03:11 +0800

    modify the naming style

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -43,7 +43,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username'
     homeConfigurations = {
-      "geoffrey" = home-manager.lib.homeManagerConfiguration {
+      geoffrey = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
